135311a2ac40984566e40e4a3d438463751dd099
jhtc5898 <33676340+jhtc5898@users.noreply.github.com>
2020-06-04 11:16:24 -0500

    segunda subida 11,16

--- tenesacaJohn_Multiprocessing.py
+++ tenesacaJohn_Multiprocessing.py
@@ -7,4 +7,135 @@ Created on Thu Jun  4 09:38:36 2020
 """
 
 
-asdsad
+import random
+
+import time
+import multiprocessing
+
+ 
+
+ 
+
+def how_many_max_values_sequential(ar):
+
+
+    #find max value of the list
+
+    maxValue = 0
+
+    for i in range(len(ar)):
+
+        if i == 0:
+
+            maxValue = ar[i]
+
+        else:
+
+            if ar[i] > maxValue:
+
+                maxValue = ar[i]
+
+    #find how many max values are in the list
+
+    contValue = 0
+
+    for i in range(len(ar)):
+
+        if ar[i] == maxValue:
+
+            contValue += 1
+
+ 
+
+    return contValue
+
+ 
+
+# Complete the how_many_max_values_parallel function below.
+
+def how_many_max_values_parallel(ar):
+ 
+    datos=[0,10,20,30,40]
+    p = multiprocessing.Pool(5)
+    #p.map(multi, datos)
+    #p.close()
+    #p.join()
+    print(p.map(multip, ar))
+    return 0
+
+def multip(datos):
+
+
+    #find max value of the list
+
+    maxValue = 0
+
+    for i in range(len(ar)):
+
+        if i == 0:
+
+            maxValue = ar[i]
+
+        else:
+
+            if ar[i] > maxValue:
+
+                maxValue = ar[i]
+
+    #find how many max values are in the list
+
+    contValue = 0
+
+    for i in range(len(ar)):
+
+        if ar[i] == maxValue:
+
+            contValue += 1
+
+ 
+
+    return contValue
+    
+
+
+
+    
+
+
+
+if __name__ == '__main__':
+
+   
+
+   
+
+    ar_count = 50
+    
+
+   
+
+    #Generate ar_count random numbers between 1 and 30
+
+    ar = [random.randrange(1,30) for i in range(ar_count)]
+
+    inicioSec = time.time()
+
+    resultSec = how_many_max_values_sequential(ar)
+
+    finSec =  time.time()
+
+   
+
+    inicioPar = time.time()   
+
+    resultPar = how_many_max_values_parallel(ar)
+
+    finPar = time.time()   
+
+   
+
+    print('Results are correct!\n' if resultSec == resultPar else 'Results are incorrect!\n')
+
+    print('Sequential Process took %.3f ms with %d items\n' % ((finSec - inicioSec)*1000, ar_count))
+
+    print('Parallel Process took %.3f ms with %d items\n' % ((finPar - inicioPar)*1000, ar_count))
